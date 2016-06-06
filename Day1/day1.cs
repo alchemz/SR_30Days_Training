78f883e1ce46c5daa2905edc634ec3cd6add1727
xianling zhang <xianlingzhang6@gmail.com>
2016-06-06 15:09:10 +0800

    Update day1.cs

--- Day1/day1.cs
+++ Day1/day1.cs
@@ -5,15 +5,16 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Speech.Recognition;
 
-namespace WindowsFormsApplication1
+namespace Test_Speech_Form
 {
     public partial class Form1 : Form
     {
         SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
-
+       
         public Form1()
         {
             InitializeComponent();
@@ -22,54 +23,45 @@ namespace WindowsFormsApplication1
         private void btnEnable_Click(object sender, EventArgs e)
         {
             recEngine.RecognizeAsync(RecognizeMode.Multiple);
-            btnDisable.Enabled= true;
+            btnDisable.Enabled = true;
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-
-            // Create a new SpeechRecognitionEngine instance.
             SpeechRecognizer recognizer = new SpeechRecognizer();
 
-            // Create a simple grammar that recognizes "red", "green", or "blue".
             Choices commands = new Choices();
-            commands.Add(new string[] { "red", "blue", "green" ,"print my name"});
+            commands.Add(new string[] { "say hello", "print my name"});
 
             // Create a GrammarBuilder object and append the Choices object.
-            GrammarBuilder gb = new GrammarBuilder();
-            gb.Append(commands);
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
 
             // Create the Grammar instance and load it into the speech recognition engine.
-            Grammar g = new Grammar(gb);
-            recEngine.LoadGrammarAsync(g);
+            Grammar grammar = new Grammar(gBuilder);
+            recEngine.LoadGrammarAsync(grammar);
             recEngine.SetInputToDefaultAudioDevice();
             recEngine.SpeechRecognized += recEngine_SpeechRecognized;
             // Register a handler for the SpeechRecognized event.
-            
         }
-
-        // Create a simple handler for the SpeechRecognized event.
         void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             switch (e.Result.Text)
             {
-                case "red":
-                MessageBox.Show("Red);
+                case "say hello":
+                    MessageBox.Show("Hello, how are you?");
                 break;
 
                 case "print my name":
-                richTextBox1.Text += "\nJohnny";
-                break;
-
+                    richTextBox1.Text += "\nJohnny";
+                    break;
             }
-            throw new NotImplementedException();
-            MessageBox.Show("Speech recognized: " + e.Result.Text);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnDisable_Click(object sender, EventArgs e)
         {
-            rectEngine.RecognizeAsyncStop();
-            btnDisable.Enabled=false;
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
         }
-    }
+    } 
 }
-
